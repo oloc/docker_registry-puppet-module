9c70ab4f9bdcbe91c7339d3046fdf5d3e2e05ec9
oloc <oloc@aurore>
2015-09-22 18:04:38 +0200

    Apt key full fingerprint.

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -21,12 +21,12 @@ class docker_registry::repos {
       }  
 
       apt::source { 'docker':
-        location   => 'https://get.docker.com/ubuntu',
+        location    => 'https://get.docker.com/ubuntu',
         include_src => false,
-        repos      => 'docker main',
-        release => '',
-        key               => 'A88D21E9',
-        key_server        => 'keyserver.ubuntu.com',
+        repos       => 'docker main',
+        release     => '',
+        key         => '36A1D7869245C8950F966E92D8576A8BA88D21E9',
+        key_server  => 'keyserver.ubuntu.com',
      }
 
      file { '/sbin/insserv' : 
