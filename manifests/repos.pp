bc811081069cbaf05c27f70491b5cf7529aad3bf
oloc <oloc@aurore>
2015-09-23 12:11:16 +0200

    Fat arrows alignement.

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -12,12 +12,12 @@ class docker_registry::repos {
       }
 
       apt::source { 'docker':
-        location    => 'https://get.docker.io/ubuntu',
-        release     => 'docker',
-        repos       => 'main',
-        key         => '36A1D7869245C8950F966E92D8576A8BA88D21E9',
-        key_server  => 'keyserver.ubuntu.com',
-        before      => Class['apt::update'],
+        locatio    => 'https://get.docker.io/ubuntu',
+        release    => 'docker',
+        repos      => 'main',
+        key        => '36A1D7869245C8950F966E92D8576A8BA88D21E9',
+        key_server => 'keyserver.ubuntu.com',
+        before     => Class['apt::update'],
       }
 
       file { '/sbin/insserv':
